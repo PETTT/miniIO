d7be6ca0c517b7e495bea7a9fc2594d4174c7394
Sean Ziegeler <seanzig@users.noreply.github.com>
2016-07-17 14:24:57 -0500

    cartiso: Added ADIOS Full output
    Created adiosfull.h & adiosfull.c
    Makefile: added adiosfull.* & ADIOS includes/links
    cartiso.c: added HAS_ADIOS sections

---
+++ cartiso/adiosfull.c
@@ -0,0 +1,124 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "adiosfull.h"
+#include "pdirs.h"
+
+static const int fnstrmax = 4095;
+
+void adiosfull_init(struct adiosfullinfo *nfo, 
+               char *name, MPI_Comm comm, int rank, int nprocs,
+               int tsteps, int ni, int nj, int nk, int is, int cni, int js, int cnj,
+               int ks, int cnk, float deltax, float deltay, float deltaz)
+{
+    char dirname[fnstrmax+1];
+
+    /* Make directory for output collection, all timesteps */
+    snprintf(dirname, fnstrmax, "%s_adios.d/", name);
+    mkdir1task(dirname, comm);
+
+    /* Set up struct */
+    nfo->name = name;
+    nfo->comm = comm;
+    nfo->rank = rank;
+    nfo->nprocs = nprocs;
+    nfo->tsteps = tsteps;
+    nfo->ni = ni;
+    nfo->nj = nj;
+    nfo->nk = nk;
+    nfo->is = is;
+    nfo->cni = cni;
+    nfo->js = js;
+    nfo->cnj = cnj;
+    nfo->ks = ks;
+    nfo->cnk = cnk;
+    nfo->deltax = deltax;
+    nfo->deltay = deltay;
+    nfo->deltaz = deltaz;
+    nfo->nvars = 0;
+    nfo->maxvars = 1000;
+    nfo->varnames = (char **) malloc(nfo->maxvars * sizeof(char *));
+    nfo->datas = (float **) malloc(nfo->maxvars * sizeof(float *));
+    
+    /* Set up ADIOS */ 
+    adios_init_noxml(comm);
+    adios_allocate_buffer(ADIOS_BUFFER_ALLOC_NOW, 10);
+    adios_declare_group(&nfo->gid, "cartiso", "", adios_flag_no);
+    chkdir1task(dirname, comm);
+    adios_select_method(nfo->gid, "MPI", "", dirname);
+
+    /* Define output variables */
+    adios_define_var(nfo->gid, "rank", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "tstep", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "ni", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "nj", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "nk", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "is", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "cni", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "js", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "cnj", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "ks", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "cnk", "", adios_integer, "", "", "");
+    adios_define_var(nfo->gid, "deltax", "", adios_real, "", "", "");
+    adios_define_var(nfo->gid, "deltay", "", adios_real, "", "", "");
+    adios_define_var(nfo->gid, "deltaz", "", adios_real, "", "", "");
+}
+
+void adiosfull_addvar(struct adiosfullinfo *nfo, char *varname, float *data)
+{
+    if(nfo->nvars >= nfo->maxvars)
+        return;   /* Just ignore too many variables, for now */
+    nfo->varnames[nfo->nvars] = varname;
+    nfo->datas[nfo->nvars] = data;
+    nfo->nvars++;
+    adios_define_var(nfo->gid, varname, "", adios_real, "cni,cnj,cnk",
+                     "ni,nj,nk", "is,js,ks");
+}
+
+void adiosfull_write(struct adiosfullinfo *nfo, int tstep)
+{
+    char fname[fnstrmax+1];
+    int timedigits = 4;
+    uint64_t ijkelems, groupsize, totalsize;
+    int64_t handle;
+    int ret, i;
+
+    /* Set filename */
+    snprintf(fname, fnstrmax, "%s.%0*d.bp", nfo->name, timedigits, tstep);
+
+    /* Set sizes */
+    ijkelems = (uint64_t)nfo->cni * nfo->cnj * nfo->cnk;
+    groupsize = sizeof(int) /*rank*/ + sizeof(int) /*tstep*/ + 
+                sizeof(int)*3 /*ni,nj,nk*/ + sizeof(int)*6 /*is-cnk*/ +
+                sizeof(float)*3 /*deltax,y,z*/ +
+                sizeof(float) * ijkelems * nfo->nvars;
+
+    /* Open & Write */
+    ret = adios_open(&handle, nfo->name, fname, "w", nfo->comm);
+    if(ret) {
+        fprintf(stderr, "Error opening ADIOS file: %s\n", fname);
+        return;
+    }
+    adios_group_size(handle, groupsize, &totalsize);
+
+    adios_write(handle, "rank", &nfo->rank);
+    adios_write(handle, "tstep", &tstep);
+    adios_write(handle, "ni", &nfo->ni);
+    adios_write(handle, "nj", &nfo->nj);
+    adios_write(handle, "nk", &nfo->nk);
+    adios_write(handle, "is", &nfo->is);
+    adios_write(handle, "cni", &nfo->cni);
+    adios_write(handle, "js", &nfo->js);
+    adios_write(handle, "cnj", &nfo->cnj);
+    adios_write(handle, "ks", &nfo->ks);
+    adios_write(handle, "cnk", &nfo->cnk);
+    adios_write(handle, "deltax", &nfo->deltax);
+    adios_write(handle, "deltay", &nfo->deltay);
+    adios_write(handle, "deltaz", &nfo->deltaz);
+    for(i = 0; i < nfo->nvars; i++)
+        adios_write(handle, nfo->varnames[i], nfo->datas[i]);
+
+    adios_close(handle);
+}
+
---
+++ cartiso/adiosfull.h
@@ -0,0 +1,31 @@
+
+#include <mpi.h>
+#include <adios.h>
+
+struct adiosfullinfo {
+    char *name;
+    MPI_Comm comm;
+    int rank;
+    int nprocs;
+    int tsteps;
+    int ni, nj, nk;
+    int is, cni, js, cnj, ks, cnk;
+    float deltax, deltay, deltaz;
+
+    int nvars;
+    int maxvars;
+    char **varnames;
+    float **datas;
+
+    int64_t gid;
+};
+
+void adiosfull_init(struct adiosfullinfo *nfo, 
+               char *name, MPI_Comm comm, int rank, int nprocs,
+               int tsteps, int ni, int nj, int nk, int is, int cni, int js, int cnj,
+               int ks, int cnk, float deltax, float deltay, float deltaz);
+
+void adiosfull_addvar(struct adiosfullinfo *nfo, char *varname, float *data);
+
+void adiosfull_write(struct adiosfullinfo *nfo, int tstep);
+

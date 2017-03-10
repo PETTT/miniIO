eab9adba19f8d6633f107cef7f52e6fdb9b82dbc
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2017-03-10 15:03:50 -0600

    (1) Only enable ADIOS when env ADIOS_HOME is set. (2) Compiler is set to mpicc if env CC is not set.

--- cartiso/test.sh
+++ cartiso/test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [ "$1" == adios ];then
 output="--pvtp"
-#output="--pvti"
-#output="--pvti --pvtp"
-#output="--adiosfull MPI"
+output="--pvti"
+output="--pvti --pvtp"
+output="--adiosfull MPI"
+else
+ if [ "$1" == hdf5 ];then
+    output="--hdf5i --hdf5p --hdf5i_chunk 256 256 256 --hdf5p_chunk 100"
+ else
+    echo "usage: test.sh <adios or hdf5>"
+ fi
+fi
 
 tsk="2 2 2"
 fq="2 2 2"
@@ -14,13 +22,13 @@ sge=0.55
 mpirun -np 8 ./cartiso --tasks $tsk --size $s $s $s --sigma $sg $sg $sg \
     --centertask --freq $fq --tsteps 20 --sin2gauss $output
 
-mpirun -np 8 ./cartiso --tasks $tsk --size $s $s $s --sigma $sg $sg $sg \
-    --centertask --freq $fq --tsteps 80 --tstart 20 --gaussmove $output
-
-mpirun -np 8 ./cartiso --tasks $tsk --size $s $s $s --sigma $sg $sg $sg \
-    --centertask --freq $fq --tsteps 20 --tstart 100 --gaussresize \
-    --backward --sigmaend $sge $sge $sge $output
-
-mpirun -np 8 ./cartiso --tasks $tsk --size $s $s $s --sigma $sge $sge $sge \
-    --centertask --freq $fq --tsteps 80 --tstart 120 --gaussmove --backward $output
+#mpirun -np 8 ./cartiso --tasks $tsk --size $s $s $s --sigma $sg $sg $sg \
+#    --centertask --freq $fq --tsteps 80 --tstart 20 --gaussmove $output
+#
+#mpirun -np 8 ./cartiso --tasks $tsk --size $s $s $s --sigma $sg $sg $sg \
+#    --centertask --freq $fq --tsteps 20 --tstart 100 --gaussresize \
+#    --backward --sigmaend $sge $sge $sge $output
+#
+#mpirun -np 8 ./cartiso --tasks $tsk --size $s $s $s --sigma $sge $sge $sge \
+#    --centertask --freq $fq --tsteps 80 --tstart 120 --gaussmove --backward $output
 

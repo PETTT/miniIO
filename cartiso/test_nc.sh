cb105779bec8781818f6763977a8910d9901e39d
Ward Fisher <wfisher@ucar.edu>
2017-05-15 11:25:42 -0600

    Added netcdf test script, will eventually merge into test_nc.sh

---
+++ cartiso/test_nc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+output="--nci --ncp"
+
+mpirun -np 8 ./cartiso --tasks "2 2 2" --size 256 256 256 --sigma 0.29 0.29 0.29 --centertask --freq 2 2 2 --tsteps 20 --sin2gauss $output

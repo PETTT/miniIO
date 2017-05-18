4875f6d4ef3fefc66e4dd608dee28d506ddd60a0
Ward Fisher <wfisher@ucar.edu>
2017-05-18 12:43:31 -0600

    Removed old script.

--- cartiso/test_nc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-output="--nci --ncp"
-#output="--nci"
-#output="--ncp"
-
-mpirun -np 8 ./cartiso --tasks 2 2 2 --size 256 256 256 --sigma 0.29 0.29 0.29 --centertask --freq 2 2 2 --tsteps 20 --sin2gauss $output

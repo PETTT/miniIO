f76f4d16193b6ef6b0be40e1a883d990bf3c679e
Ward Fisher <wfisher@ucar.edu>
2017-05-19 12:54:08 -0600

    Sketching in unstruct functions.

--- struct/run_hdf.sh
+++ struct/run_hdf.sh
@@ -3,5 +3,5 @@
 
 set -e
 
-mpirun --np 4 ./struct --tasks 2 2 --size 2 2 2 --tsteps 1 --hdf5
+mpirun --np 4 ./struct --tasks 2 2 --size 2 2 2 --tsteps 10 --hdf5
 #mpirun --np 4 ./struct --tasks 2 2 --size 16 12 2 --tsteps 1 --hdf5
---
+++ unstruct/tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Running HDF5 tests."
+mpirun -np 8 ./unstruct --tasks 2 2 2 --size 9 9 9 --levels 3 --tsteps 1 --hdf5
+echo ""
+echo "Runing netCDF tests."
+mpirun -np 8 ./unstruct --tasks 2 2 2 --size 9 9 9 --levels 3 --tsteps 1 --nc
+echo ""
+echo "Finished."

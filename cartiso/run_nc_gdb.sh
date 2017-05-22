e0964f507bed660d9a92f807da997a79604e334a
Ward Fisher <wfisher@ucar.edu>
2017-05-22 16:54:36 -0600

    More netCDF miniapp cleanup, as well as documentation update.

--- cartiso/run_nc_gdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Utility script to easily run a netCDF file.
-
-set -e
-
-mpirun --np 8 xterm -bg white -fg black -e gdb -x gdb.txt ./cartiso
-#mpirun --np 4 xterm -e "gdb ./struct --tasks 2 2 --size 2 2 2 --tsteps 1 --nc"
-#mpirun --np 4 ./struct --tasks 2 2 --size 16 12 2 --tsteps 1 --nc
--- unstruct/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Running HDF5 tests."
-mpirun -n 8 ./unstruct --points 24 --hdf5
-echo ""
-echo "Runing netCDF tests."
-mpirun -n 8 ./unstruct --points 24 --nc
-echo ""
-echo "Finished."

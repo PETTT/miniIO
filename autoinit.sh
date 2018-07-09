a8f76fe9e6c6038cf667e0901f238cdeba3edd60
Sean Ziegeler <seanzig@users.noreply.github.com>
2018-07-09 03:40:14 +0000

    Better opt handling in Makefile.inc/autoinit.sh

--- autoinit.sh
+++ autoinit.sh
@@ -6,13 +6,23 @@
 if [ -n "$BC_HOST" ]; then
 
     export CC=mpicc
+    export OPT="-O3 -xhost -ip"     # Assumes intel compiler
 
     ## All Cray's using the CrayPE compiler environment
     if [ -n "$CRAYPE_DIR" ]; then
 
         echo "Using DSRC Cray with CrayPE configuration"
-        echo "   Current environment: `echo $LOADEDMODULES | grep -Po 'PrgEnv-.*?/'`"
+        prgenv=`echo $LOADEDMODULES | grep -Po 'PrgEnv-.*?/'`
+        echo "   Current environment: $prgenv"
         export CC=cc     # CrayPE compiler wrapper
+        if [ "$prgenv" == "PrgEnv-intel/" ]; then
+            export OPT="-O3 -xhost -ip"
+        elif [ "$prgenv" == "PrgEnv-gnu/" ]; then
+            export OPT="-O3"
+        else
+            export OPT="-O2"
+        fi
+        echo "   Set OPT=$OPT"
 
     ## Topaz/SGI
     elif [ "$BC_HOST" == "topaz" ]; then
@@ -30,9 +40,14 @@ elif [ -n "$POD_PBSSERVERS" ]; then
 
     echo "Using default POD configuration"
     export CC=mpicc
+    export OPT="-O3 -xhost -ip"     # Assumes intel compiler
 
 ### Nothing else to try
 else 
-    echo "No special configuration detected - doing nothing."
+    echo "No special configuration detected"
+    if [ -n "`which mpicc`" ]; then
+        export CC=mpicc
+        echo "   Found mpicc - setting CC=$CC"
+    fi
 fi
 

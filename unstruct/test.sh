539dd38b02f8f7ec164f8e99d93bfa6dc66396db
Scot Breitenfeld <brtnfld@hdfgroup.org>
2022-02-10 15:00:13 -0600

    updated testing script with new arg. conventions

--- unstruct/test.sh
+++ unstruct/test.sh
@@ -10,7 +10,7 @@ NT=2
 output="--hdf5"
 mpirun -np 8 ./unstruct --pointspertask $PTS --tsteps $NT $output
 
-output="--hdf5 --hdf5_chunk 1 1 1 --hdf5_compress gzip,9"
+output="--hdf5 --hdf5_chunk 18 --hdf5_compress gzip,9"
 mpirun -np 8 ./unstruct --pointspertask $PTS --tsteps $NT $output
 
 
